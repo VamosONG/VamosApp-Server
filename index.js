b90f719fec2da75c16b17baf7661b8faf097901e
Fishlennon <jpaviles5@gmail.com>
2024-01-16 22:08:29 -0300

    port

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const server = require("./src/server");
 const { conn } = require('./src/dataBase');
 
 
-const port = process.env.PORT || 10000;
+const port = process.env.PORT || 3001;
+const host = '0.0.0.0' ; 
 
 conn.sync({ force: false }).then(() => {
-server.listen(port, () => {
+server.listen(port, host, () => {
   console.log(`Server listening on port ${port}`);
 })
 }).catch(error => console.error(error))
